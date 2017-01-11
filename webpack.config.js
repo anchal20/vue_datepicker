d59bc90793e41450c6b6f50a30850f146e6b283d
Anchal <anchalsachdeva20@gmail.com>
2017-01-11 15:35:42 +0530

    webpack config + sample vue program

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './app/main.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  }
+}
