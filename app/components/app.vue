f1550df6a738bce80890fe6583ef8ea61b4fdbed
Anchal <anchalsachdeva20@gmail.com>
2017-02-06 17:30:35 +0530

    first vue app with webpack

---
+++ app/components/app.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="">
+    {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hello World'
+    }
+  },
+	render: h => h(App)
+}
+</script>
+
+<style lang="css">
+</style>
